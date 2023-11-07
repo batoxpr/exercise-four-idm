2d4d5039ee35b4f1aa825c4de2abb12b55e91d60
Bato Batbileg <batoxpr@10-18-230-179.dynapool.wireless.nyu.edu>
2023-11-07 09:51:37 -0500

    update01

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("About page")
+    res.send("hi, this is the about page")
 });
 
 module.exports = router;
